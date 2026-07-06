d44befb11ed25e39a5d60b043c37d13a0e0a78f4
Mohit Kumar Pandey <mohitpandey8792@gmail.com>
2026-07-06 15:35:35 +0530

    Created all required UI components

--- SocialMedia/src/App.jsx
+++ SocialMedia/src/App.jsx
@@ -1,11 +1,23 @@
-
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Sidebar from "./components/Siderbar"
+import CreatePost from "./components/CreatePost"
+import PostList from "./components/PostList"
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-     
-    </>
+    <div className="app-container">
+     <Sidebar></Sidebar>
+     <div className="content">
+        <Header></Header>
+        <CreatePost></CreatePost>
+        <PostList></PostList>
+        <Footer></Footer>
+
+     </div>
+    
+    </div>
   )
 }
 
